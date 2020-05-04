aa2301d9eeb2db5fe839821695718de5830493d1
unknown <raya9631@gmail.com>
2020-05-03 22:03:28 -0500

    modelos, crud y detalles esteticos

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,3 +1,10 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('reptile') }}'><i class='nav-icon fa fa-question'></i> Reptiles</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('post') }}'><i class='nav-icon fa fa-question'></i> Posts</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('reptile') }}'><i class='nav-icon fa fa-frog'></i> Reptiles</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('post') }}'><i class='nav-icon fa fa-edit'></i> Posts</a></li>
+<li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon fa fa-id-card-alt"></i> Autenticación</a>
+	<ul class="nav-dropdown-items">
+	<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon fa fa-user"></i> <span>Usuarios</span></a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon fa fa-user-tag"></i> <span>Roles</span></a></li>
+    </ul>
+</li>
