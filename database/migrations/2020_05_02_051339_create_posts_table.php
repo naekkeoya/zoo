a20af98d233c6cac8b50562766c3174de1c8a99a
unknown <raya9631@gmail.com>
2020-05-09 17:51:30 -0500

    correccion de migracion post

--- database/migrations/2020_05_02_051339_create_posts_table.php
+++ database/migrations/2020_05_02_051339_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -32,6 +32,6 @@ class CreatePostTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post');
+        Schema::dropIfExists('posts');
     }
 }
